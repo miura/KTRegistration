0307fb1e4284947bfbc2ceccef767f741b76561c
Kota Miura <miura@embl.de>
2013-06-13 19:31:25 +0200

    ImxParser added with meny methods to extract specific nodes, updating spot coordinates and writing the updated nodes to a new Imx file

--- src/main/java/de/embl/cmci/xml/ImxParser.java
+++ src/main/java/de/embl/cmci/xml/ImxParser.java
@@ -13,11 +13,18 @@ package de.embl.cmci.xml;
  */
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
 import org.w3c.dom.Document;
 import org.w3c.dom.NamedNodeMap;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Node;
 import org.w3c.dom.Element;
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -31,12 +38,68 @@ public class ImxParser {
 	boolean ref1found = false;
 	boolean ref2found = false;	
 	double[] ref1 = new double[3];
-	double[] ref2 = new double[3];	
+	double[] ref2 = new double[3];
 	
+	/**
+	 * current <spots><spot> Nodes 
+	 */
+	NodeList spotlist;
+	
+	/**
+	 * current <bpTrack>...<spot> Nodes
+	 * for the step 1, this will be empty. 
+	 */
+	NodeList tspotlist;
+	/**
+	 *  the initial list of spots. (cloned) 
+	 */
+	ArrayList<Node> spotlistOrg;
+	
+	/**
+	 *  the list of registered spots.  (cloned) 
+	 */	
+	ArrayList<Node> spotlistReg;
+	
+	/**
+	 * the list of registered back spots (not containing track-spots)  (cloned) 
+	 */
+	ArrayList<Node> spotlistRegBack;
+	
+	/**
+	 * the list of spots within tracks  (cloned) 
+	 */
+	ArrayList<Node> spotlistTracks;
+
+	/**
+	 * the list of <bpTrack>s under <bpSurfaceComponents>
+	 */
+	NodeList trackList;
+	/**
+	 * a clone of the loaded xml, to be over written and saved as a new xml file. 
+	 */
+	public Document doc;
+	/**
+	 * true if the loaded file seems to be spot only file (the first step)
+	 */
+	public boolean isSpotfile;
+	/**
+	 * true if the loaded file seems to be already tracked file (the second step)
+	 */
+	public boolean isTrackedFile;
+	private String rootpath;
+	
+	
+	/** Document Loader
+	 *  see http://www.mkyong.com/java/how-to-read-xml-file-in-java-dom-parser/
+	 * @param filepath
+	 * @return
+	 */
 	public Document parseImx(String filepath){
 		Document doc = null;
 		try {
 			File imxFile = new File(filepath);
+			this.rootpath = imxFile.getParent();
+			
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
 			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
 			doc = dBuilder.parse(imxFile);
@@ -48,24 +111,106 @@ public class ImxParser {
 	    }
 		return doc;
 	}
-	// initial run to retireve spot information. 
+//	// initial run to retireve spot information. 
+//	public void loadImaxInfoOld(String filepath){
+//		//ImxParser ip = new ImxParser();
+//		timepoints = new ArrayList<Integer>();
+//		positions = new ArrayList<double[]>();
+//		Document doc = this.parseImx(filepath);	
+//		NodeList nList = doc.getElementsByTagName("spot");
+//		for (int i = 0; i < nList.getLength(); i++) {
+//			Node nNode = nList.item(i);
+//			Element eElement = (Element) nNode;
+//			timepoints.add(Integer.parseInt(eElement.getAttribute("time")));
+//			String[] pos = eElement.getAttribute("position").split(" ");
+//			double[] elem = new double[4];
+//			for (int j = 0; j < 3; j++)
+//				elem[j] = Double.parseDouble(pos[j]);
+//			elem[3] = Double.parseDouble(eElement.getAttribute("time"));
+//			positions.add(elem);
+//		}
+//		Object maxT = Collections.max(timepoints);
+//		Object minT = Collections.min(timepoints);
+//		maxtime = (Integer) maxT;
+//		mintime = (Integer) minT;
+//		if (maxtime > mintime)
+//			frames =maxtime - mintime + 1;
+//		else
+//			System.out.println(" something wrong with imx file: timepoint min >= max");
+//		System.out.println("minT:" + Integer.toString(mintime));
+//		System.out.println("maxT:" + Integer.toString(maxtime));
+//		
+//	}
+	
 	public void loadImaxInfo(String filepath){
 		//ImxParser ip = new ImxParser();
 		timepoints = new ArrayList<Integer>();
 		positions = new ArrayList<double[]>();
-		Document doc = this.parseImx(filepath);	
-		NodeList nList = doc.getElementsByTagName("spot");
-		for (int i = 0; i < nList.getLength(); i++) {
-			Node nNode = nList.item(i);
-			Element eElement = (Element) nNode;
-			timepoints.add(Integer.parseInt(eElement.getAttribute("time")));
-			String[] pos = eElement.getAttribute("position").split(" ");
-			double[] elem = new double[4];
-			for (int j = 0; j < 3; j++)
-				elem[j] = Double.parseDouble(pos[j]);
-			elem[3] = Double.parseDouble(eElement.getAttribute("time"));
-			positions.add(elem);
+		Document docorg = this.parseImx(filepath);
+		Document doc = (Document) docorg.cloneNode(true);
+		this.doc = doc;
+		
+		boolean hasSpotList = false;
+		boolean hasTrackSpots = false;
+		
+		//get spots under <spots>, spots without tracking. 
+		NodeList spots = getSpots(doc); //spots under <spots>
+		System.out.println("spot number:" + spots.getLength());
+		if (spots.getLength() > 0)
+			hasSpotList = true;
+		
+		Node tracksContainer = null;
+		NodeList trackspots = null;
+		NodeList tracklist = null;
+		tracksContainer = getBPTracks(doc);
+		if (tracksContainer != null){
+			trackspots = getSpots( (Element) tracksContainer);
+			trackList = getTracks((Element) tracksContainer);
+			if (trackspots != null){
+				System.out.println("track spot number:" + trackspots.getLength());
+				hasTrackSpots = true;
+			} else
+				System.out.println("No spots in tracks");
+		} else {
+			System.out.println("No Tracks file");
 		}
+		if (hasSpotList && !hasTrackSpots)
+			isSpotfile = true;
+		else if (hasSpotList && hasTrackSpots)
+			isTrackedFile = true;
+
+		//step1
+		if (isSpotfile){
+			this.spotlist = spots;
+			this.spotlistOrg = nodeList2ArrayList(spots);
+			
+		//step2	
+		} else if (isTrackedFile) {
+			this.spotlist = spots;
+			this.tspotlist = trackspots;
+			this.spotlistReg = nodeList2ArrayList(spots);
+			this.spotlistTracks = nodeList2ArrayList(trackspots);
+			this.trackList = tracklist;
+		}
+		
+		parseSpotsList(spots);
+		
+	}
+	ArrayList<Node> nodeList2ArrayList(NodeList src){
+		ArrayList<Node> al = new ArrayList<Node>();
+		for (int i = 0; i < src.getLength(); i ++)
+			al.add(src.item(i).cloneNode(false)); 
+		return al;
+	}
+	/**
+	 * parse time points and get min and max of time points. 
+	 * store positions as a String array in the order of the appearance of the <spot>
+	 * @param spots
+	 */
+	void parseSpotsList(NodeList spots){
+		this.positions = spotNodeList2ArrayList(spots);
+		for (double[] spot : this.positions) 
+			timepoints.add( (int) spot[3]);		
 		Object maxT = Collections.max(timepoints);
 		Object minT = Collections.min(timepoints);
 		maxtime = (Integer) maxT;
@@ -75,9 +220,83 @@ public class ImxParser {
 		else
 			System.out.println(" something wrong with imx file: timepoint min >= max");
 		System.out.println("minT:" + Integer.toString(mintime));
-		System.out.println("maxT:" + Integer.toString(maxtime));
-		
+		System.out.println("maxT:" + Integer.toString(maxtime));		
 	}
+	
+	ArrayList<double[]> spotNodeList2ArrayList(NodeList spots){
+		ArrayList<double[]> positions = new ArrayList<double[]>();
+		for (int i = 0; i < spots.getLength(); i++) {
+			Node nNode = spots.item(i);
+			Element eElement = (Element) nNode;
+			String[] pos = eElement.getAttribute("position").split(" ");
+			double[] elem = new double[5];
+			for (int j = 0; j < 3; j++)
+				elem[j] = Double.parseDouble(pos[j]);
+			elem[3] = Double.parseDouble(eElement.getAttribute("time"));
+			positions.add(elem);
+		}
+		return positions;
+	}
+
+	double[][] convertSpotPos (){
+		// index = 4 in the original ImxRegister is index within string array (rec)
+		// but for here, a dummy (will be 0 for all)
+		double[][] pos = new double[this.positions.size()][5];
+		for (int i = 0; i < pos.length; i++)
+			pos[i] = this.positions.get(i);
+		return pos;
+	}
+	/**
+	 * Using the double[][] (Tomo's format of positions) passed to the argument, 
+	 * updates the doc content, <spots><spot> lines.  
+	 * @param pos
+	 * @param spotlist
+	 * @return
+	 */
+	boolean setSpotList(double[][] pos, NodeList spotlist){
+		if (pos.length != spotlist.getLength()){
+			System.out.println("Given double[][] has different lenght as spotlistReg");
+			return false;
+		}
+		for (int i = 0; i < pos.length; i++){
+			String posstring = Double.toString(pos[i][0]) +
+					" " + Double.toString(pos[i][1]) + 
+					" " + Double.toString(pos[i][2]) ;
+			Node spot = spotlist.item(i);
+			NamedNodeMap attrs = spot.getAttributes();
+			Node attpos = attrs.getNamedItem("position");
+			attpos.setTextContent(posstring);
+		}
+		return true;
+	}
+	
+	/** 
+	 * for step1. registered spot positions will be overwritten. 
+	 * @param pos
+	 * @return
+	 */
+	public boolean setSpotListReg(double[][] pos){
+		if (setSpotList(pos, this.spotlist)){
+			this.spotlistReg = nodeList2ArrayList(this.spotlist);
+			return true;
+		} else
+			return false;
+	}
+
+	/** 
+	 * for step2. registered back spot positions will be overwritten. 
+	 * This method updates only the spots under <spots><spot>
+	 * @param pos
+	 * @return
+	 */
+	public boolean setSpotListRegBack(double[][] pos){
+		if (setSpotList(pos, this.spotlist)){
+			this.spotlistRegBack = nodeList2ArrayList(this.spotlist);
+			return true;
+		} else
+			return false;
+	}	
+	
 	
 	/**
 	 * Collects track information for RegisterBack.
@@ -196,11 +415,80 @@ public class ImxParser {
 			}
 		}		
 	}
-	double[][] convertSpotPos (){
-		double[][] pos = new double[this.positions.size()][4];
-		for (int i = 0; i < pos.length; i++)
-			pos[i] = this.positions.get(i);
-		return pos;
+	
+	Node getbpSurfaceComponent(Document doc){
+		Node bpSurfaceComponent = null;
+		NodeList nl = doc.getElementsByTagName("bpSurfaceComponent");
+		if (nl.getLength() != 1)
+			System.out.println("<bpSurfaceComponent> tags =" + Integer.toString(nl.getLength()));
+		else {
+			bpSurfaceComponent = nl.item(0);
+		}		
+		return bpSurfaceComponent;		
 	}
 	
+	// returns the node bpSurfaceComponent just above bpTracks
+	Node getBPTracks(Document doc){
+		NodeList bpTrack = doc.getElementsByTagName("bpTrack");
+		System.out.println("<bpTrack> tags =" + Integer.toString(bpTrack.getLength()));
+		Node trackContainer = null;
+		if (bpTrack.getLength() > 0) 
+			trackContainer = bpTrack.item(0).getParentNode();
+		return trackContainer;
+	}
+	//returns list of <spot> nodes
+	NodeList getSpots(Document doc){
+		NodeList spotsnode  = doc.getElementsByTagName("spots");
+		NodeList spots = null;
+		if ((spotsnode.getLength() < 1) || (spotsnode == null) ){
+			System.out.println(" no <spots> found>");
+		} else if (spotsnode.getLength() > 1){
+			System.out.println(" more than one <spots> nodes found>");
+		} else {
+			Element spotelement = (Element) spotsnode.item(0);
+			spots = getSpots(spotelement);
+		}
+		return spots;
+	}	
+
+	//returns a NodeList of <spot> nodes
+	NodeList getSpots(Element e){
+		NodeList spots  = e.getElementsByTagName("spot");
+		if ((spots.getLength() < 1) || (spots == null) ){
+			System.out.println(" no <spot> found>");
+		} else {
+			System.out.println(" more than one <spot> nodes found>");
+		}
+		return spots;
+	}
+	//list of <bpTrack>
+	NodeList getTracks(Element e){
+		NodeList tracks  = e.getElementsByTagName("bpTrack");
+		if ((tracks.getLength() < 1) || (tracks == null) ){
+			System.out.println(" no <bpTrack> found>");
+		} else {
+			System.out.println("<bpTrack>tags:" + Integer.toString(tracks.getLength()));
+		}
+		return tracks;
+	}
+	
+	/**
+	 * writes updated xml as a new file. 
+	 * http://www.mkyong.com/java/how-to-modify-xml-file-in-java-dom-parser/
+	 * @param filepath fullpath to a new file to be written.
+	 */
+	public void writeUpdatedImx(String filepath){
+		try {
+			// write the content into xml file
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource source = new DOMSource(doc);
+			StreamResult result = new StreamResult(new File(filepath));
+			transformer.transform(source, result);
+			System.out.println("Done");
+		} catch (TransformerException tfe) {
+			tfe.printStackTrace();
+		}
+		
+	}
 }
