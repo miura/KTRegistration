c54d93af11b21661f146bfb084e1665961ee1fd1
Kota Miura <miura@embl.de>
2013-06-13 20:44:08 +0200

    a bit more cleaning of ImxRegister

--- src/main/java/de/embl/cmci/xml/ImxRegister.java
+++ src/main/java/de/embl/cmci/xml/ImxRegister.java
@@ -1,7 +1,5 @@
 package de.embl.cmci.xml;
 
-
-
 import java.awt.*;
 import java.awt.event.*;
 import java.io.BufferedWriter;
@@ -125,20 +123,13 @@ public class ImxRegister extends WindowAdapter implements ActionListener{
 		String fullpath = folder + orif;
 		ouputInfo(fullpath);
 		
-		//String imx = loadImx(folder+orif);
 		ImxParser ip = new ImxParser();
 		ip.loadImaxInfo(fullpath);
 		
 		//int nTime = 31;
 		int nTime = ip.frames;
-
-		//String[] rec = splitImxToString(imx);
-
-		// never used?
-		// out, on 20130613 double refPos[][] = loadRefPos(rec, nTime);
 		
 		// ------ spots <spot>
-		//double spotsPos[][] = loadSpotsPos(rec, nTime);
 		double spotsPos[][] = ip.convertSpotPos();
 		
 		String spotsinfo = "\n === Spots Info === \n";
@@ -157,16 +148,14 @@ public class ImxRegister extends WindowAdapter implements ActionListener{
 			centroidinfo += String.format(placeholder, i, cenPos[i][0], cenPos[i][1], cenPos[i][2]);
 		ouputInfo(centroidinfo);
 
-		//	cenPos = refPos;
 
-//		double allSpotPos[][] = loadAllSpotPos(rec, nTime);
-//		double registeredPos[][] = registerPos(allSpotPos, cenPos);
 		double registeredPos[][] = registerPos(spotsPos, cenPos);
 		
-//		String[] registeredRec = replaceIntoRegistered(rec, registeredPos);
 		ip.setSpotListReg(registeredPos);
+
+		// === writing files ===
 		outputCenPos(cenPos, folder, orif);
-//		outputRegisteredImx(registeredRec, folder, orif);
+
 		String newfilepath = folder + "registered-"+ orif;
 		ip.writeUpdatedImx(newfilepath);
 		
@@ -185,21 +174,13 @@ public class ImxRegister extends WindowAdapter implements ActionListener{
 		System.exit(0);
 	}
 
-	private static void outputRegisteredImx(String[] registeredRec, String folder, String f) {
-		try{
-			File file = new File(folder + "registered-"+f);
-			PrintWriter pw
-				= new PrintWriter(new BufferedWriter(new FileWriter(file)));
-			for(int i=0;i<registeredRec.length;i++){
-				pw.print(registeredRec[i]+" ");
-			}
-			pw.close();
-		}catch(IOException e){
-			System.out.println(e);
-		}
-
-	}
-
+	/**
+	 * Writes centorid positions in a file. 
+	 * @author Tomo
+	 * @param cenPos
+	 * @param folder
+	 * @param f
+	 */
 	private static void outputCenPos(double[][] cenPos, String folder, String f) {
 		try{
 			File file2 = new File(folder + "cenPos-"+f+".csv");
@@ -220,19 +201,13 @@ public class ImxRegister extends WindowAdapter implements ActionListener{
 
 	}
 
-	private static String[] replaceIntoRegistered(String[] rec,
-			double[][] registeredPos) {
-		int i;
-		String[] registeredRec = rec;
-		for(i=0;i<registeredPos.length;i++){
-			int j = (int)registeredPos[i][4];
-			registeredRec[j+1] = "position=\"" + registeredPos[i][0];
-			registeredRec[j+2] = ""+ registeredPos[i][1];
-			registeredRec[j+3] = registeredPos[i][2] + "\"";
-		}
-		return registeredRec;
-	}
-
+	/**
+	 * Registers spots by centroid of all spots / time frame. 
+	 * @author Tomo
+	 * @param allSpotPos
+	 * @param cenPos
+	 * @return
+	 */
 	private static double[][] registerPos(double[][] allSpotPos,
 			double[][] cenPos) {
 		int i,j;
@@ -252,35 +227,14 @@ public class ImxRegister extends WindowAdapter implements ActionListener{
 		return registeredPos;
 	}
 
-	private static double[][] loadAllSpotPos(String[] rec, int time) {
-		int i,n;
-		n=0;
-
-		for(i=0;i<rec.length;i++){
-			if(rec[i].equals("<spot")){
-				n++;
-				i = i+4;
-			}
-		}
-
-		double[][] pos = new double[n][5];
-
-		int k=0;
-		for(i=0;i<rec.length;i++){
-			if(rec[i].equals("<spot")){
-				pos[k][0] = xFromStr(rec[i+1]);
-				pos[k][1] = yFromStr(rec[i+2]);
-				pos[k][2] = zFromStr(rec[i+3]);
-				pos[k][3] = timeFromStr(rec[i+4]);
-				pos[k][4] = i;
-				i = i+4;
-				k++;
-			}
-		}
-
-		return pos;
-	}
-
+	
+	/**
+	 * Computes centorid / time frame
+	 * @author Tomo
+	 * @param pos
+	 * @param nTime
+	 * @return
+	 */
 	private static double[][] calcCenPos(double[][] pos, int nTime) {
 		double[][] cenPos = new double[nTime][3];
 		int i=0;
@@ -334,186 +288,92 @@ public class ImxRegister extends WindowAdapter implements ActionListener{
 
 	}
 
-	private static double[][] loadSpotsPos(String[] rec, int nTime) {
-
-		int i,n;
-		boolean inSpots = false;
-
-		n=0;
-
-		for(i=0;i<rec.length;i++){
-			if(rec[i].equals("<spots>"+System.getProperty("line.separator"))){
-				inSpots = true;
-				System.out.println("spotsHere");
-			}
-
-			if(inSpots && rec[i].equals("<spot")){
-				n++;
-				i = i+4;
-			}
-			if(rec[i].length()>8){
-				if(inSpots && rec[i].substring(0,8).equals("</spots>")){
-					inSpots = false;
-					i = rec.length;
-					System.out.println("spotsOut");
-				}
-			}
-		}
-
-		double[][] pos = new double[n][4];
-		int k=0;
-		for(i=0;i<rec.length;i++){
-			if(rec[i].equals("<spots>"+System.getProperty("line.separator"))){
-				inSpots = true;
-				System.out.println("spots");
-			}
-
-			if(inSpots && rec[i].equals("<spot")){
-
-				pos[k][0] = xFromStr(rec[i+1]);
-				pos[k][1] = yFromStr(rec[i+2]);
-				pos[k][2] = zFromStr(rec[i+3]);
-				pos[k][3] = timeFromStr(rec[i+4]);
-				i = i+4;
-				k++;
-			}
-			if(rec[i].length()>8){
-				if(inSpots && rec[i].substring(0,8).equals("</spots>")){
-					inSpots = false;
-					i = rec.length;
-				}
-			}
-		}
-
-		return pos;
-
-	}
-
-	private static double[][] loadRefPos(String[] rec, int nTime) {
-		double[][] refPos = new double[nTime][3];
-		int i,t;
-		int n=rec.length;
-		boolean inRef = false;
-
-		for(i=0;i<n;i++){
-			if(rec[i].equals("<name>referencetrack</name>"+System.getProperty("line.separator"))){
-				inRef = true;
-				System.out.println("reference");
-			}
-
-			if(inRef && rec[i].equals("<spot")){
-				t = timeFromStr(rec[i+4]);
-				refPos[t][0] = xFromStr(rec[i+1]);
-				refPos[t][1] = yFromStr(rec[i+2]);
-				refPos[t][2] = zFromStr(rec[i+3]);
-				i = i+4;
-			}
-			if(rec[i].length()>10){
-				if(inRef && rec[i].contains("</bpTrack>")){
-					inRef = false;
-					i = n;
-					System.out.println("bptrackHere");
-				}
-			}
 
 
-		}
-
-		return refPos;
-	}
-
-	private static double xFromStr(String str) {
-		int i;
-		double x;
-		char[] strChar = str.toCharArray();
-		char[] tChar = new char[strChar.length-10];
-		for(i=10;i<strChar.length; i++){
-			tChar[i-10]=strChar[i];
-		}
-		String nStr = new String(tChar);
-		x = Double.parseDouble(nStr);
-		return x;
-	}
-	private static double yFromStr(String str) {
-		int i;
-		double y;
-		char[] strChar = str.toCharArray();
-		char[] tChar = new char[strChar.length];
-		for(i=0;i<strChar.length; i++){
-			tChar[i]=strChar[i];
-		}
-		String nStr = new String(tChar);
-		y = Double.parseDouble(nStr);
-		return y;
-	}
-
-	private static double zFromStr(String str) {
-		int i;
-		double z;
-		char[] strChar = str.toCharArray();
-		char[] tChar = new char[strChar.length-1];
-		for(i=0;i<strChar.length-1; i++){
-			tChar[i]=strChar[i];
-		}
-		String nStr = new String(tChar);
-		z = Double.parseDouble(nStr);
-		return z;
-	}
-
-
-
-	private static int timeFromStr(String str) {
-		int i,t;
-		char[] strChar;
-		strChar = str.toCharArray();
-		char[] tChar = new char[strChar.length-7];
-		for(i=6;i<strChar.length-1; i++){
-			tChar[i-6]=strChar[i];
-		}
-		String nStr = new String(tChar);
-		t = Integer.parseInt(nStr);
-		return t;
-	}
-
-	private static String[] splitImxToString(String s) {
-
-
-		String[] strrec = s.split(" ");
-
-		return strrec;
-
-
-	}
-
-	/** for loading imx file as string. 
-	 *  
-	 * @param filename
-	 * @return file contents as string. 
-	 */
-	private static String loadImx(String filename) {
-		String s = null;
-		try
-		{
-			File f = new File(filename);
-			byte[] b = new byte[(int) f.length()];
-			FileInputStream fi = new FileInputStream(f);
-			fi.read(b);
-			s = new String(b);
-			fi.close();
-		}
-		catch(Exception e)
-		{
-			return null;
-
-		}
-		if ( s	==	null )
-		{
-			return null;
-		}
-
-		return s;
-
-
-	}
+//	private static double[][] loadRefPos(String[] rec, int nTime) {
+//		double[][] refPos = new double[nTime][3];
+//		int i,t;
+//		int n=rec.length;
+//		boolean inRef = false;
+//
+//		for(i=0;i<n;i++){
+//			if(rec[i].equals("<name>referencetrack</name>"+System.getProperty("line.separator"))){
+//				inRef = true;
+//				System.out.println("reference");
+//			}
+//
+//			if(inRef && rec[i].equals("<spot")){
+//				t = timeFromStr(rec[i+4]);
+//				refPos[t][0] = xFromStr(rec[i+1]);
+//				refPos[t][1] = yFromStr(rec[i+2]);
+//				refPos[t][2] = zFromStr(rec[i+3]);
+//				i = i+4;
+//			}
+//			if(rec[i].length()>10){
+//				if(inRef && rec[i].contains("</bpTrack>")){
+//					inRef = false;
+//					i = n;
+//					System.out.println("bptrackHere");
+//				}
+//			}
+//
+//
+//		}
+//
+//		return refPos;
+//	}
+//
+//	private static double xFromStr(String str) {
+//		int i;
+//		double x;
+//		char[] strChar = str.toCharArray();
+//		char[] tChar = new char[strChar.length-10];
+//		for(i=10;i<strChar.length; i++){
+//			tChar[i-10]=strChar[i];
+//		}
+//		String nStr = new String(tChar);
+//		x = Double.parseDouble(nStr);
+//		return x;
+//	}
+//	private static double yFromStr(String str) {
+//		int i;
+//		double y;
+//		char[] strChar = str.toCharArray();
+//		char[] tChar = new char[strChar.length];
+//		for(i=0;i<strChar.length; i++){
+//			tChar[i]=strChar[i];
+//		}
+//		String nStr = new String(tChar);
+//		y = Double.parseDouble(nStr);
+//		return y;
+//	}
+//
+//	private static double zFromStr(String str) {
+//		int i;
+//		double z;
+//		char[] strChar = str.toCharArray();
+//		char[] tChar = new char[strChar.length-1];
+//		for(i=0;i<strChar.length-1; i++){
+//			tChar[i]=strChar[i];
+//		}
+//		String nStr = new String(tChar);
+//		z = Double.parseDouble(nStr);
+//		return z;
+//	}
+//
+//
+//
+//	private static int timeFromStr(String str) {
+//		int i,t;
+//		char[] strChar;
+//		strChar = str.toCharArray();
+//		char[] tChar = new char[strChar.length-7];
+//		for(i=6;i<strChar.length-1; i++){
+//			tChar[i-6]=strChar[i];
+//		}
+//		String nStr = new String(tChar);
+//		t = Integer.parseInt(nStr);
+//		return t;
+//	}
 
 }
