c6aa746fc4aa891fdf15c63aa4d80d6adb275689
Kota Miura <miura@embl.de>
2013-06-13 20:44:51 +0200

    a bit more cleaning of ImxRegister 2

--- src/main/java/de/embl/cmci/xml/ImxRegister.java
+++ src/main/java/de/embl/cmci/xml/ImxRegister.java
@@ -4,7 +4,6 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
